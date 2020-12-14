c171055209f53f3875e9341d13e7fe04c464a2f1
tuanna <tuannguyenanh1412@gmail.com>
2020-12-14 12:51:49 +0700

    fix ui

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
+const path = require('path')
 const app = express();
 
 app.use(express.static('build'));
 
 app.get('*', (req, res) => {
-  res.send(__dirname + '/build/index.html');
+  res.send(path.resolve(__dirname, './build/index.html'));
 })
 
 app.listen(process.env.PORT || 8000);
